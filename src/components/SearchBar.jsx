863cc8ab801510c9f70ca213183462d280471c6c
cyan-byte <62404444+cyan-byte@users.noreply.github.com>
2023-11-17 20:47:34 -0500

    changed background

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ export default function SearchBar() {
   return (
     <div className='search-bar'>Type in a word
     {/* add search input tag and search functionality here */}
-
+    <img src='magnifying-glass-icon.png' alt='search icon'/>
     </div>
   )
 }
